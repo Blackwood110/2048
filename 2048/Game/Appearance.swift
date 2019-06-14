c721b96a2b9b7dd018bf5e21e4cac6834d54bf9c
Сашка <aleksandrdergelev@mail.ru>
2019-06-14 14:47:25 +0300

    lesson2

--- 2048/Game/Appearance.swift
+++ 2048/Game/Appearance.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2019 Александр Дергилёв. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol AppearanceProtocol: class {
+    func tileColor(value: Int) -> UIColor
+    func numberColor(value: Int) -> UIColor
+    func fontForNumbers() -> UIFont
+}
+
+class Appearance: AppearanceProtocol {
+    func tileColor(value: Int) -> UIColor {
+        switch value {
+        case 2:
+            return UIColor(displayP3Red: 238.0/255.0, green: 228.0/255.0, blue: 218.0/255.0, alpha: 1.0)
+        case 4:
+            return UIColor(displayP3Red: 237.0/255.0, green: 224.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+        case 8:
+            return UIColor(displayP3Red: 242.0/255.0, green: 177.0/255.0, blue: 121.0/255.0, alpha: 1.0)
+        case 16:
+            return UIColor(displayP3Red: 245.0/255.0, green: 149.0/255.0, blue: 99.0/255.0, alpha: 1.0)
+        case 32:
+            return UIColor(displayP3Red: 246.0/255.0, green: 124.0/255.0, blue: 95.0/255.0, alpha: 1.0)
+        case 64:
+            return UIColor(displayP3Red: 246.0/255.0, green: 94.0/255.0, blue: 59.0/255.0, alpha: 1.0)
+        case 128, 256, 512, 1024, 2048:
+            return UIColor(displayP3Red: 237.0/255.0, green: 207.0/255.0, blue: 114.0/255.0, alpha: 1.0)
+        default:
+            return UIColor.white
+        }
+    }
+    
+    func numberColor(value: Int) -> UIColor {
+        switch value {
+        case 2,4:
+            return UIColor(displayP3Red: 119.0/255.0, green: 110.0/255.0, blue: 101.0/255.0, alpha: 1.0)
+        default:
+            return UIColor.white
+        }
+    }
+    
+    func fontForNumbers() -> UIFont {
+        if let font = UIFont(name: "HelveticaNeue-Bold", size: 20) {
+            return font
+        }
+        return UIFont.systemFont(ofSize: 20)
+    }
+}
