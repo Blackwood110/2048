23b4725b1fe2a857028a17a0dbca882e2f8722e8
Сашка <aleksandrdergelev@mail.ru>
2019-06-14 14:57:22 +0300

    lesson3

--- 2048/Views/Accessory.swift
+++ 2048/Views/Accessory.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2019 Александр Дергилёв. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol ScoreViewProtocol {
+    func scoreChanged(newScore s: Int)
+}
+
+class ScoreView: UIView, ScoreViewProtocol {
+    var score: Int = 0 {
+        didSet {
+            label.text = "SCORE: \(score)"
+        }
+    }
+    let defaultFrame = CGRect(x: 0, y: 0, width: 140, height: 140)
+    var label: UILabel
+    
+    init(backgroundColor bgColor: UIColor, textColor tColor: UIColor, font: UIFont, radius r: CGFloat) {
+        label = UILabel(frame: defaultFrame)
+        label.textAlignment = NSTextAlignment.center
+        super.init(frame: defaultFrame)
+        
+        backgroundColor = bgColor
+        label.textColor = tColor
+        label.font = font
+        
+        layer.cornerRadius = r
+        self.addSubview(label)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func scoreChanged(newScore s: Int) {
+        let defaultFrame = CGRect(x: 0, y: 0, width: 140, height: 140)
+    }
+}
