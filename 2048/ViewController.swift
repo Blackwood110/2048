0c3a6316468353acc866866dac46197dfa9bfc4a
Сашка <aleksandrdergelev@mail.ru>
2019-06-14 13:41:41 +0300

    Initial Commit

---
+++ 2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Александр Дергилёв on 14/06/2019.
+//  Copyright © 2019 Александр Дергилёв. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
